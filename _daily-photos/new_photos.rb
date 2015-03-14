0af6db2fe6acd50fba1a0c044457fb68d22189a9
Evan Davis <evan.wingfield@me.com>
2015-03-14 11:32:46 -0700

    more photos, better script

--- _daily-photos/new_photos.rb
+++ _daily-photos/new_photos.rb
@@ -1,6 +1,16 @@
+#!/usr/bin/ruby
 require "date"
 
-ARGV.each do |day_of_year|
+file_count = 0
+Dir.glob(Dir.pwd + '/*.md') do |rb_file|
+  file_count = file_count + 1
+end
+
+new_file_count = ARGV[0].to_i
+i = 1
+
+while i <= new_file_count
+	day_of_year = file_count + i
 	date_string = Date.strptime("2015-#{day_of_year}", "%Y-%j").to_s
 	out_file = File.new("a#{day_of_year}.md", "w")
 	out_file.puts "---\n"
@@ -11,9 +21,8 @@ ARGV.each do |day_of_year|
 	out_file.puts "short-description: \"Description\""
 	out_file.puts "---"
 	out_file.close
+	i = i+1
 end
-
-
 
 # ---
 # title:  "001/365"
